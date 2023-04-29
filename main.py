f609e15c8cd4e8948c426bdaba8995024fdce904
Evan <evango123@outlook.com>
2023-04-29 23:26:41 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# hello_psg.py
+
+import PySimpleGUI as sg
+
+layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+margins = (500,100))
+
+# Create the window
+window = sg.Window("Demo", layout)
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
