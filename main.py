8b753650aa4619daf73e218ebc063a60759f94a1
Evan <evango123@outlook.com>
2023-04-30 00:01:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 # hello_psg.py
 
 import PySimpleGUI as sg
+import os
 
-layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
-margins = (500,100))
+layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("adb devices")]]
 
 # Create the window
-window = sg.Window("Demo", layout)
+window = sg.Window("Demo", layout, margins=(100,100))
 
 # Create an event loop
 while True:
     event, values = window.read()
     # End program if user closes window or
     # presses the OK button
-    if event == "OK" or event == sg.WIN_CLOSED:
+    if event == "adb devices":
+        os.system("cd C:\adb\")
+        os.system("adb devices")
+
+    elif event == sg.WIN_CLOSED:
         break
 
 window.close()
