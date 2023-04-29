e352368fa7caa57656422aa597a7d5f6e4552d9f
Evan <evango123@outlook.com>
2023-04-30 00:42:35 +0100

    add close button and change window text

--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 import PySimpleGUI as sg
 import os
 
-layout = [[sg.Text("ADBtools")], [sg.Button("adb devices")]]
+layout = [[sg.Text("ADBtools")], [sg.Button("adb devices")], [sg.Button("close")]]
 
 # Create the window
-window = sg.Window("Demo", layout, margins=(100,100))
+window = sg.Window("ADBtools", layout, margins=(100,100))
 
 # Create an event loop
 while True:
@@ -16,6 +16,9 @@ while True:
     if event == "adb devices":
         os.chdir("adb")
         os.system("adb devices")
+
+    elif event == "close":
+        break
 
     elif event == sg.WIN_CLOSED:
         break
