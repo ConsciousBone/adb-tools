4011f675abc3622b74d4ffced072b4cde6bf1b6e
Evan <evango123@outlook.com>
2023-04-30 00:09:14 +0100

    fix main.py and add adb files

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import PySimpleGUI as sg
 import os
 
-layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("adb devices")]]
+layout = [[sg.Text("ADBtools")], [sg.Button("adb devices")]]
 
 # Create the window
 window = sg.Window("Demo", layout, margins=(100,100))
@@ -14,7 +14,7 @@ while True:
     # End program if user closes window or
     # presses the OK button
     if event == "adb devices":
-        os.system("cd C:\adb\")
+        os.chdir("adb")
         os.system("adb devices")
 
     elif event == sg.WIN_CLOSED:
