89cacbeff982cb4213298966c69ec23d3ef034de
Evan <evango123@outlook.com>
2023-04-30 00:47:51 +0100

    tidy up and add explanation

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-# hello_psg.py
+# set up
 
 import PySimpleGUI as sg
 import os
 
 layout = [[sg.Text("ADBtools")], [sg.Button("adb devices")], [sg.Button("close")]]
 
-# Create the window
+# add explanation
+
+print("This is the output window.")
+print("Everything that would normally be printed into a terminal goes here.")
+print('')
+
+# create window
 window = sg.Window("ADBtools", layout, margins=(100,100))
 
-# Create an event loop
+# create loop
 while True:
     event, values = window.read()
     # End program if user closes window or
