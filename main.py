1edecfb015925292b9a27740b841c9bade41e494
Evan <evango123@outlook.com>
2023-04-30 02:19:04 +0100

    added custom command shit

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 import PySimpleGUI as sg
 import os
 
-sg.theme("DarkGrey2")
+sg.theme("DarkGrey6")
 
 layout = [ [sg.Text("ADBtools")],
            [sg.Text('')],
            [sg.Text("Run commands"), sg.Button("adb devices"), ],
-           [sg.Text("Custom adb command:"), sg.Text("adb"), sg.InputText()],
+           [sg.Text("Custom adb command:"), sg.Text("adb"), sg.InputText(), sg.Button("Send")],
            [sg.Button("close ADBtools")] ]
 
 
@@ -20,7 +20,7 @@ print("Everything that would normally be printed into a terminal goes here.")
 print('')
 
 # create window
-window = sg.Window("ADBtools", layout, margins=(100,100))
+window = sg.Window("ADBtools", layout, margins=(25,25))
 
 # create loop
 while True:
@@ -32,9 +32,11 @@ while True:
         os.system("adb devices")
 
     elif event == "close ADBtools":
+        print("ADBtools closed")
         break
 
     elif event == sg.WIN_CLOSED:
+        print("ADBtools closed")
         break
 
 window.close()
